b212ef68cebaac7397c4409c243917b2eea466a5
Nikita Gromadskyi <njck_g@yahoo.com>
2020-03-10 19:27:36 +0200

    Update Currency.swift

--- kantor/Model/Currency/Currency.swift
+++ kantor/Model/Currency/Currency.swift
@@ -1,5 +1,20 @@
 import Foundation
 
+// TODO: When can avoid enumarating each currency and generilize for better scaling in future:
+/* struct CurrencyExchange {
+    let name: String
+    let base: Currency
+    let sell: [Currency]
+    let buy: [Currency]
+   }
+
+   struct Currency {
+       let name: String // "USD", "EUR", "UAH"
+       let code: Int // could be arbitrary id like 1,2...
+       let price: Double
+   }
+*/
+// custom sorting could be done by `code` or `name`
 struct Currency {
     let name: String
     let usdSell: String
